c6c6ffe157e59710619534b4a061b45fe1458e48
sanghee <sanghee0610@ajou.ac.kr>
2021-10-12 00:35:51 +0900

    Add Admin Project & Modify Model Project

---
+++ UMate/UMateModel/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                builder.UseSqlServer("Server=tcp:practice1008.database.windows.net,1433;Initial Catalog=UMate;Persist Security Info=False;User ID=test;Password=abcd1234@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+    }
+}

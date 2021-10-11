10174e0c6d2372d95e2e5c5080949db981258a59
sanghee <sanghee0610@ajou.ac.kr>
2021-10-12 00:58:09 +0900

    [Add - UMateModel/Models] CommonResponse.cs, ResultCode.cs 추가

---
+++ UMate/UMateModel/Models/CommonResponse.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace UMateModel.Models
+{
+    public class CommonResponse
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ UMate/UMateModel/Models/ResultCode.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace UMateModel.Models
+{
+    public class ResultCode
+    {
+        public static int Ok = 200;
+        public static int Fail = -999;
+    }
+}

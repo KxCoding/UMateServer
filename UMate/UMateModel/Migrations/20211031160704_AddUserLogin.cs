9b63ec495615f3090797da9f319014406aeac03c
sanghee <sanghee0610@ajou.ac.kr>
2021-11-01 01:09:22 +0900

    [Add] 유저 로그인 Migration 추가 및 DB 업데이트

---
+++ UMate/UMateModel/Migrations/20211031160704_AddUserLogin.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UMateModel.Migrations
+{
+    public partial class AddUserLogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "JoinDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "NickName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SelectedProfileImage",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UniversityConfirmed",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UniversityName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdateDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "YearOfAdmission",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JoinDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NickName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "SelectedProfileImage",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UniversityConfirmed",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UniversityName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "YearOfAdmission",
+                table: "AspNetUsers");
+        }
+    }
+}

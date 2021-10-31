d7a871575e8c34e8921e0278185ce1231cc10eb6
Hyunwoo <heoun3089@gmail.com>
2021-11-01 00:51:16 +0900

    [Account - Add] ApplicationUser 모델 추가

--- UMate/UMateModel/Entities/LectureInfo/LectureReview.cs
+++ UMate/UMateModel/Entities/LectureInfo/LectureReview.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace UMateModel.Entities.LectureInfo
---
+++ UMate/UMateModel/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace UMateModel.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string NickName { get; set; }
+
+        public string YearOfAdmission { get; set; }
+        public string UniversityName { get; set; }
+        public bool UniversityConfirmed { get; set; }
+
+        public string SelectedProfileImage { get; set; }
+
+        public DateTime JoinDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}

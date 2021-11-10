c5c5e67cd46745bbbd1ddba0fa1665527e945351
sanghee <sanghee0610@ajou.ac.kr>
2021-11-10 22:28:40 +0900

    [Board - Add] Board 모델 Migration 추가 및 DB 업데이트

---
+++ UMate/UMateModel/Migrations/20211110131917_BoardModel.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UMateModel.Migrations
+{
+    public partial class BoardModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Board",
+                columns: table => new
+                {
+                    BoardId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Section = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Board", x => x.BoardId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Professor",
+                columns: table => new
+                {
+                    ProfessorId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professor", x => x.ProfessorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BoardId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.CategoryId);
+                    table.ForeignKey(
+                        name: "FK_Category_Board_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Board",
+                        principalColumn: "BoardId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    BoardId = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Content = table.Column<string>(nullable: false),
+                    LikeCnt = table.Column<int>(nullable: false),
+                    CommentCnt = table.Column<int>(nullable: false),
+                    ScrapCnt = table.Column<int>(nullable: false),
+                    CategoryNumber = table.Column<int>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Post_Board_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Board",
+                        principalColumn: "BoardId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LectureInfo",
+                columns: table => new
+                {
+                    LectureInfoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProfessorId = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    BookName = table.Column<string>(nullable: true),
+                    BookLink = table.Column<string>(nullable: true),
+                    Semesters = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LectureInfo", x => x.LectureInfoId);
+                    table.ForeignKey(
+                        name: "FK_LectureInfo_Professor_ProfessorId",
+                        column: x => x.ProfessorId,
+                        principalTable: "Professor",
+                        principalColumn: "ProfessorId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    PostId = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(nullable: false),
+                    LikeCnt = table.Column<int>(nullable: false),
+                    OriginalCommentId = table.Column<int>(nullable: false),
+                    IsReComment = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_Comment_Post_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Post",
+                        principalColumn: "PostId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LikePost",
+                columns: table => new
+                {
+                    LikePostId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    PostId = table.Column<int>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LikePost", x => x.LikePostId);
+                    table.ForeignKey(
+                        name: "FK_LikePost_Post_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Post",
+                        principalColumn: "PostId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PostImage",
+                columns: table => new
+                {
+                    PostImageId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostId = table.Column<int>(nullable: false),
+                    UrlString = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostImage", x => x.PostImageId);
+                    table.ForeignKey(
+                        name: "FK_PostImage_Post_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Post",
+                        principalColumn: "PostId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScrapPost",
+                columns: table => new
+                {
+                    ScrapPostId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    PostId = table.Column<int>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScrapPost", x => x.ScrapPostId);
+                    table.ForeignKey(
+                        name: "FK_ScrapPost_Post_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Post",
+                        principalColumn: "PostId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LectureReview",
+                columns: table => new
+                {
+                    LectureReviewId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    LectureInfoId = table.Column<int>(nullable: false),
+                    Assignment = table.Column<int>(nullable: false),
+                    GroupMeeting = table.Column<int>(nullable: false),
+                    Evaluation = table.Column<int>(nullable: false),
+                    Attendance = table.Column<int>(nullable: false),
+                    TestNumber = table.Column<int>(nullable: false),
+                    Rating = table.Column<int>(nullable: false),
+                    Semester = table.Column<string>(nullable: false),
+                    Content = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LectureReview", x => x.LectureReviewId);
+                    table.ForeignKey(
+                        name: "FK_LectureReview_LectureInfo_LectureInfoId",
+                        column: x => x.LectureInfoId,
+                        principalTable: "LectureInfo",
+                        principalColumn: "LectureInfoId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TestInfo",
+                columns: table => new
+                {
+                    TestInfoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    LectureInfoId = table.Column<int>(nullable: false),
+                    Semester = table.Column<string>(nullable: false),
+                    TestType = table.Column<string>(nullable: false),
+                    TestStrategy = table.Column<string>(nullable: false),
+                    QuestionTypes = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestInfo", x => x.TestInfoId);
+                    table.ForeignKey(
+                        name: "FK_TestInfo_LectureInfo_LectureInfoId",
+                        column: x => x.LectureInfoId,
+                        principalTable: "LectureInfo",
+                        principalColumn: "LectureInfoId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LikeComment",
+                columns: table => new
+                {
+                    LikeCommentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    CommentId = table.Column<int>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LikeComment", x => x.LikeCommentId);
+                    table.ForeignKey(
+                        name: "FK_LikeComment_Comment_CommentId",
+                        column: x => x.CommentId,
+                        principalTable: "Comment",
+                        principalColumn: "CommentId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Example",
+                columns: table => new
+                {
+                    ExampleId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TestInfoId = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Example", x => x.ExampleId);
+                    table.ForeignKey(
+                        name: "FK_Example_TestInfo_TestInfoId",
+                        column: x => x.TestInfoId,
+                        principalTable: "TestInfo",
+                        principalColumn: "TestInfoId",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_BoardId",
+                table: "Category",
+                column: "BoardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_PostId",
+                table: "Comment",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Example_TestInfoId",
+                table: "Example",
+                column: "TestInfoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LectureInfo_ProfessorId",
+                table: "LectureInfo",
+                column: "ProfessorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LectureReview_LectureInfoId",
+                table: "LectureReview",
+                column: "LectureInfoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LikeComment_CommentId",
+                table: "LikeComment",
+                column: "CommentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LikePost_PostId",
+                table: "LikePost",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_BoardId",
+                table: "Post",
+                column: "BoardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostImage_PostId",
+                table: "PostImage",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScrapPost_PostId",
+                table: "ScrapPost",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestInfo_LectureInfoId",
+                table: "TestInfo",
+                column: "LectureInfoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "Example");
+
+            migrationBuilder.DropTable(
+                name: "LectureReview");
+
+            migrationBuilder.DropTable(
+                name: "LikeComment");
+
+            migrationBuilder.DropTable(
+                name: "LikePost");
+
+            migrationBuilder.DropTable(
+                name: "PostImage");
+
+            migrationBuilder.DropTable(
+                name: "ScrapPost");
+
+            migrationBuilder.DropTable(
+                name: "TestInfo");
+
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "LectureInfo");
+
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.DropTable(
+                name: "Professor");
+
+            migrationBuilder.DropTable(
+                name: "Board");
+        }
+    }
+}

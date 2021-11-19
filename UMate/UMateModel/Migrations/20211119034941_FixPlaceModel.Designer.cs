5ce1514b4b61c37ee80a12a7c32ae7305c32f915
sanghee <sanghee0610@ajou.ac.kr>
2021-11-19 12:53:38 +0900

    [Place - Fix] 모델 수정 Migration & DB Update

--- UMate/UMateModel/Migrations/20211119034941_FixPlaceModel.Designer.cs
+++ UMate/UMateModel/Migrations/20211119034941_FixPlaceModel.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UMateModel.Contexts;
 
 namespace UMateModel.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211119034941_FixPlaceModel")]
+    partial class FixPlaceModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UMate/UMateModel/Migrations/20211119034941_FixPlaceModel.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UMateModel.Migrations
+{
+    public partial class FixPlaceModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "portal",
+                table: "University",
+                newName: "Portal");
+
+            migrationBuilder.RenameColumn(
+                name: "map",
+                table: "University",
+                newName: "Map");
+
+            migrationBuilder.RenameColumn(
+                name: "library",
+                table: "University",
+                newName: "Library");
+
+            migrationBuilder.RenameColumn(
+                name: "homepage",
+                table: "University",
+                newName: "Homepage");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Portal",
+                table: "University",
+                newName: "portal");
+
+            migrationBuilder.RenameColumn(
+                name: "Map",
+                table: "University",
+                newName: "map");
+
+            migrationBuilder.RenameColumn(
+                name: "Library",
+                table: "University",
+                newName: "library");
+
+            migrationBuilder.RenameColumn(
+                name: "Homepage",
+                table: "University",
+                newName: "homepage");
+        }
+    }
+}

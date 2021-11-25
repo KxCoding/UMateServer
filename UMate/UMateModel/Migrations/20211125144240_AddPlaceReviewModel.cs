c8443cfc82187ffa64c0cb4645dbebbf5a9a0914
sanghee <sanghee0610@ajou.ac.kr>
2021-11-25 23:44:04 +0900

    [Add] 상점리뷰모델 Migration 추가 및 DB Update

---
+++ UMate/UMateModel/Migrations/20211125144240_AddPlaceReviewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UMateModel.Migrations
+{
+    public partial class AddPlaceReviewModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlaceReview",
+                columns: table => new
+                {
+                    PlaceReviewId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    PlaceId = table.Column<int>(nullable: true),
+                    Taste = table.Column<string>(nullable: false),
+                    Service = table.Column<string>(nullable: false),
+                    Mood = table.Column<string>(nullable: false),
+                    Price = table.Column<string>(nullable: false),
+                    Amount = table.Column<string>(nullable: false),
+                    StarRating = table.Column<double>(nullable: false),
+                    ReviewText = table.Column<string>(nullable: false),
+                    RecommendationCount = table.Column<int>(nullable: false),
+                    InsertDate = table.Column<DateTime>(nullable: false),
+                    UpdateDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlaceReview", x => x.PlaceReviewId);
+                    table.ForeignKey(
+                        name: "FK_PlaceReview_Place_PlaceId",
+                        column: x => x.PlaceId,
+                        principalTable: "Place",
+                        principalColumn: "PlaceId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlaceReview_PlaceId",
+                table: "PlaceReview",
+                column: "PlaceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlaceReview");
+        }
+    }
+}

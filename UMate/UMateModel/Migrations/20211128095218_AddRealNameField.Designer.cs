436dcf9cf677deda3875f86a61da04aeca9a4ddd
sanghee <sanghee0610@ajou.ac.kr>
2021-11-28 18:53:06 +0900

    [Add] RealNameField Add migrations & DB Update

--- UMate/UMateModel/Migrations/20211128095218_AddRealNameField.Designer.cs
+++ UMate/UMateModel/Migrations/20211128095218_AddRealNameField.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UMateModel.Contexts;
 
 namespace UMateModel.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211128095218_AddRealNameField")]
+    partial class AddRealNameField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UMate/UMateModel/Migrations/20211128095218_AddRealNameField.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UMateModel.Migrations
+{
+    public partial class AddRealNameField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RealName",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RealName",
+                table: "AspNetUsers");
+        }
+    }
+}
